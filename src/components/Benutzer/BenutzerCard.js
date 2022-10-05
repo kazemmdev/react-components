5dc0b0e022cb5d0bf884300243b42d1d6badc291
Kazem Mirzaei <k90mirzaei@gmail.com>
2022-10-05 16:22:39 +0800

    remove option selector components

--- src/components/Benutzer/BenutzerCard.js
+++ src/components/Benutzer/BenutzerCard.js
@@ -12,16 +12,52 @@ const data = [
     anschrift: "Firma QFS GmbH",
     funktion: "Prufer",
   },
+  {
+    benutzer: "1000-1015",
+    anrede: "Herr",
+    nachname: "Benai 2",
+    vorname: "Raschid",
+    password: "12345",
+    mail: "benai@example.de",
+    anschrift: "Firma QFS GmbH",
+    funktion: "Prufer",
+  },
 ];
 
 export class BenutzerCard extends Component {
   state = {
-    selected: [],
+    selected: null,
     items: data,
+    options: [
+      {
+        key: "anrede",
+        isOpen: false,
+        values: ["Herr", "Frau"],
+      },
+      {
+        key: "funktion",
+        isOpen: false,
+        values: ["Prufer"],
+      },
+    ],
   };
 
   render() {
-    const { selected, items } = this.state;
+    const { selected, items, options } = this.state;
+
+    const toggleOption = (name) => {
+      const { options } = this.state;
+      const index = options.findIndex((item) => item.key === name);
+      options[index].isOpen = !options[index].isOpen;
+      this.setState({ options });
+    };
+
+    const setOptionValue = (name, select) => {
+      const { selected } = this.state;
+      if (selected && selected[name]) selected[name] = select;
+      this.setState({ selected });
+      toggleOption(name);
+    };
 
     return (
       <div className={styles.card}>
@@ -30,10 +66,34 @@ export class BenutzerCard extends Component {
             <img src={require("../../assets/plus-small.svg").default} alt="plus" />
           </div>
           <div className="mr-6">Benutzer</div>
-          <Options
-            items={items.map((item) => item.nachname)}
-            selectOption={(e) => this.setState({ selected: items.find((item) => item.nachname === e) })}
-          />
+          <div className={styles.optionsWrapper}>
+            <button
+              type="button"
+              className={styles.optionsInput}
+              onClick={() => this.setState({ isOpen: !this.state.isOpen })}
+            >
+              {selected?.nachname}
+              <span className={styles.optionsInputSvg}>
+                <img src={require("../../assets/menu.svg").default} />
+              </span>
+            </button>
+            <ul className={styles.optionsMenu + (this.state.isOpen ? " block" : " hidden")}>
+              {items.map((item) => (
+                <li
+                  key={item.benutzer}
+                  className={styles.optionsItem}
+                  onClick={() =>
+                    this.setState({
+                      selected: items.find((x) => x.benutzer === item.benutzer),
+                      isOpen: false,
+                    })
+                  }
+                >
+                  {item.nachname}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className={styles.inputs}>
           <div className={styles.inputRow}>
@@ -42,11 +102,36 @@ export class BenutzerCard extends Component {
           </div>
           <div className={styles.inputRow}>
             <div className={styles.inputName}>Anrede</div>
-            <Options
-              items={["Herr", "Frau"]}
-              value={selected?.anrede || ""}
-              selectOption={(e) => this.setState({ selected: { ...selected, anrede: e } })}
-            />
+            <div className={styles.optionsWrapper}>
+              <button
+                type="button"
+                className={styles.optionsInput}
+                onClick={() => toggleOption("anrede")}
+              >
+                {selected?.anrede}
+                <span className={styles.optionsInputSvg}>
+                  <img src={require("../../assets/menu.svg").default} />
+                </span>
+              </button>
+              <ul
+                className={
+                  styles.optionsMenu +
+                  (options.find((item) => item.key === "anrede").isOpen ? " block" : " hidden")
+                }
+              >
+                {options
+                  .find((item) => item.key === "anrede")
+                  .values.map((item) => (
+                    <li
+                      key={item}
+                      className={styles.optionsItem}
+                      onClick={() => setOptionValue("anrede", item)}
+                    >
+                      {item}
+                    </li>
+                  ))}
+              </ul>
+            </div>
           </div>
           <div className={styles.inputRow}>
             <div className={styles.inputName}>Nachname</div>
@@ -55,7 +140,9 @@ export class BenutzerCard extends Component {
               className={styles.inputField}
               placeholder="Nachname"
               value={selected?.nachname || ""}
-              onChange={(e) => this.setState({ selected: { ...selected, nachname: e.target.value } })}
+              onChange={(e) =>
+                this.setState({ selected: { ...selected, nachname: e.target.value } })
+              }
             />
           </div>
           <div className={styles.inputRow}>
@@ -65,7 +152,9 @@ export class BenutzerCard extends Component {
               className={styles.inputField}
               placeholder="Vorname"
               value={selected?.vorname || ""}
-              onChange={(e) => this.setState({ selected: { ...selected, vorname: e.target.value } })}
+              onChange={(e) =>
+                this.setState({ selected: { ...selected, vorname: e.target.value } })
+              }
             />
           </div>
           <div className={styles.inputRow}>
@@ -85,16 +174,43 @@ export class BenutzerCard extends Component {
               className={styles.inputField}
               placeholder="Anschrift"
               value={selected?.anschrift || ""}
-              onChange={(e) => this.setState({ selected: { ...selected, anschrift: e.target.value } })}
+              onChange={(e) =>
+                this.setState({ selected: { ...selected, anschrift: e.target.value } })
+              }
             />
           </div>
           <div className={styles.inputRow}>
             <div className={styles.inputName}>Funktion</div>
-            <Options
-              items={["Prufer"]}
-              value={selected?.funktion || ""}
-              selectOption={(e) => this.setState({ selected: { ...selected, funktion: e } })}
-            />
+            <div className={styles.optionsWrapper}>
+              <button
+                type="button"
+                className={styles.optionsInput}
+                onClick={() => toggleOption("funktion")}
+              >
+                {selected?.funktion}
+                <span className={styles.optionsInputSvg}>
+                  <img src={require("../../assets/menu.svg").default} />
+                </span>
+              </button>
+              <ul
+                className={
+                  styles.optionsMenu +
+                  (options.find((item) => item.key === "funktion").isOpen ? " block" : " hidden")
+                }
+              >
+                {options
+                  .find((item) => item.key === "funktion")
+                  .values.map((item) => (
+                    <li
+                      key={item}
+                      className={styles.optionsItem}
+                      onClick={() => setOptionValue("funktion", item)}
+                    >
+                      {item}
+                    </li>
+                  ))}
+              </ul>
+            </div>
           </div>
         </div>
         <div className={styles.actions}>
@@ -102,41 +218,6 @@ export class BenutzerCard extends Component {
           <button className={styles.buttonInfo}>PW Reset</button>
           <button className={styles.buttonSuccess}>Speichern</button>
         </div>
-      </div>
-    );
-  }
-}
-
-export class Options extends Component {
-  state = {
-    isOpen: false,
-    selected: "",
-  };
-
-  render() {
-    const handleSelectOption = (item) => {
-      this.props.selectOption(item);
-      this.setState({ isOpen: false, selected: item });
-    };
-    return (
-      <div className={styles.optionsWrapper}>
-        <button
-          type="button"
-          className={styles.optionsInput}
-          onClick={() => this.setState({ isOpen: !this.state.isOpen })}
-        >
-          {this.state.selected || this.props.value}
-          <span className={styles.optionsInputSvg}>
-            <img src={require("../../assets/menu.svg").default} />
-          </span>
-        </button>
-        <ul className={styles.optionsMenu + (this.state.isOpen ? " block" : " hidden")}>
-          {this.props.items.map((item) => (
-            <li key={item} className={styles.optionsItem} onClick={() => handleSelectOption(item)}>
-              {item}
-            </li>
-          ))}
-        </ul>
       </div>
     );
   }
